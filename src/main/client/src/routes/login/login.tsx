8eefb21f456394aece42228a70e2fd1689f536fb
Kotsanai Chikosi <krcchiko@gmail.com>
2024-07-02 17:10:08 -0400

    login updates
    login updates

--- src/main/client/src/routes/login/login.tsx
+++ src/main/client/src/routes/login/login.tsx
@@ -1,32 +1,72 @@
+import * as React from "react";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import TopNav from '../top-nav/top-nav.tsx'
-import BottomNav  from '../bottom-nav/bottom-nav.tsx'
+import BottomNav from '../bottom-nav/bottom-nav.tsx'
+
+import { useSearchParams } from "react-router-dom";
+
+interface LoginUser {
+  username: string;
+  password: string;
+}
 
 export default function Login() {
-return (
-<>
-    <TopNav/>
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Sign up for email notifications" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
-    <BottomNav/>
-</>
-);
+  let [searchParams, setSearchParams] = useSearchParams();
+  let [isAuthenticated, setAuth] = React.useState(false);
+  let [loginUser, setLoginUser] = React.useState<LoginUser>();
+  
+
+  function handleChange() {
+    //console.log("handle change..")
+    let loginUser : LoginUser = {
+      username: document.querySelector("#formBasicEmail").value,
+      password: document.querySelector("#formBasicPassword").value,
+    };
+    //console.log(loginUser.username);
+    //console.log(loginUser.password);
+    setLoginUser(loginUser);
+  }
+
+  let login = () => {
+    fetch('http://localhost:8080/api/v1/signin', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(userData)
+    })
+      .then(response => setAuth(true))
+      .catch(err => console.log(err))
+  };
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log(loginUser?.username);
+    console.log(loginUser?.password);
+  }
+
+  return (
+    <>
+      <TopNav />
+      <Form onSubmit={handleSubmit} className="login">
+        <Form.Group className="mb-3" controlId="formBasicEmail" onChange={handleChange}>
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword" onChange={handleChange}>
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Sign up for email notifications" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+      <BottomNav />
+    </>
+  );
 }
--- src/main/client/src/routes/root.tsx
+++ src/main/client/src/routes/root.tsx
@@ -1,16 +1,13 @@
-import TopNav from './top-nav/top-nav.tsx'
 import Home from './home-page.tsx'
-import BottomNav  from './bottom-nav/bottom-nav.tsx'
+
 import '../index.css'
 
 export default function Root() {
   return (
     <>
-    <TopNav/>
-    <div id="root">
-        <Home/>
-    </div>
-    <BottomNav/>
+      <div id="root">
+        <Home />
+      </div>
     </>
   );
 }
